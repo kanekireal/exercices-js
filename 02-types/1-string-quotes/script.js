7396b99705cf31bf4c2588f8691f037276560c3f
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-12-10 19:13:56 +0100

    Finish exercice [02-1] and totally [02]

--- 02-types/1-string-quotes/script.js
+++ 02-types/1-string-quotes/script.js
@@ -2,3 +2,18 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+// Nous assignons la valeur "Ilya" à la variable "name".
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+// Ce dernier affichera une alerte, avec comme valeur :
+// -> hello 1
+
+alert( `hello ${"name"}` ); // ?
+// Ce dernier affichera une alerte, avec comme valeur :
+// -> hello name
+
+alert( `hello ${name}` ); // ?
+// Ce dernier affichera une alerte, avec comme valeur :
+// -> hello Ilya
