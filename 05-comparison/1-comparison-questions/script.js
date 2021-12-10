1ec69c27ba325c59c8c6868c9093e7e56125c522
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-12-10 21:37:34 +0100

    Finish exercice [05-1] and totally [05]

--- 05-comparison/1-comparison-questions/script.js
+++ 05-comparison/1-comparison-questions/script.js
@@ -2,3 +2,29 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+
+//Transcription: 5 (Nombre) > 4 (Nombre)
+const A = 5 > 4;
+// (true)
+
+//Transcription: 1 (Character : a) > 16 (Character : p)
+const B = "apple" > "pineapple";
+// (false)
+
+//Transcription: 2 (Character : 2) > 1 (Character: 1)
+const C = "2" > "12";
+// (true)
+
+//Transcription: 0 == 0
+const D = undefined == null;
+// (true)
+//Transcription: (typeof undefined) == (typeof null) && 0 == 0
+const E = undefined === null;
+// (false)
+
+const F = null == "\n0\n";
+// (false)
+
+const G = null === +"\n0\n";
+// (false)
