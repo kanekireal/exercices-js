e773f0f9b3487771b47fd51086558656ed4e04d4
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-12-10 22:34:11 +0100

    Finish totally [8] and [9]

--- 09-switch/1-rewrite-switch-if-else/script.js
+++ 09-switch/1-rewrite-switch-if-else/script.js
@@ -2,3 +2,11 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+if (browser === 'Edge') {
+    alert("You've got the Edge!");
+} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    alert('Okay we support these browsers too');
+} else {
+    alert('We hope that this page looks ok!');
+}
