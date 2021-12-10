94da120ff83da672d0c0f3974c23e360b55f2d16
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-12-10 19:27:19 +0100

    Finish exercice [03-1] and totally [03]

--- 03-alert-prompt-confirm/1-simple-page/script.js
+++ 03-alert-prompt-confirm/1-simple-page/script.js
@@ -2,3 +2,12 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+const valideFirstName = (firstName) => Number(firstName) === 0 ? null : firstName;
+
+let firstName;
+
+do firstName = valideFirstName(prompt('Quel est votre prénom ?')) ?? 'Anonyme';
+while (!confirm(`Êtes vous sur que votre prénom est "${firstName}"`));
+
+alert(`Bonjour ${firstName} ☺️ !`);
