080508097dd334f0c5cdf8a8ac8c7ea0bbbfa717
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-12-10 18:51:26 +0100

    Finish exercice [01-2]

--- 01-variables/2-declare-variables/script.js
+++ 01-variables/2-declare-variables/script.js
@@ -2,3 +2,22 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+// Les normes/conventions souhaite que lorsqu'on nomme une variable (pas seulement...) en JavaScript (ansi que d'autres langages de programmation).
+// Nous utilisions la notation "camelCase", "lowerCamelCase" (plusieurs façons de nommé cette notation).
+// Et de préférence une notation en anglais.
+
+// Cette notation consiste à écrire un ensemble de mots.
+// En les liants sans espace, sans ponctuation.
+// Et en mettant la première lettre de chaque mot en capital, sauf pour le premier.
+
+// Dans ce cas-là, une "planète" en anglais "planet" est en un seul mot.
+// Donc il n'y pas besoin de modification.
+let planet = "Terre";
+
+// Dans ces cas-ci, prenons "Prénom" en anglais "first name".
+// Ce dernier est composé de deux mots, le premier étant "first" et le second "name".
+// Nous transformeront donc "name" en "Name", et supprimerons l'espace entre les deux.
+// Ce qui nous donne comme ci-dessous "firstName".
+let firstName = "Wesley";
+let lastName = "Levasseur";
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ ___
 # _Liste des exercices :_
 1. [ ] [Variables](01-variables) :
     - [x] [Hello variables](01-variables/1-hello-variables)
-    - [ ] [Declare variable](01-variables/2-declare-variables)
+    - [x] [Declare variable](01-variables/2-declare-variables)
     - [ ] [Uppercast constant](01-variables/3-uppercast-constant)
 2. [ ] [Types](02-types)
     - [ ] [String quotes](02-types/1-string-quotes)
